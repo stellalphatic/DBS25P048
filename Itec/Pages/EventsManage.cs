e9a232a1d0b6d551ab3a024d33c72e61f275bb12
Shahzaib Ali <163044429+stellalphatic@users.noreply.github.com>
2025-02-28 20:14:16 +0500

    updated event management page

---
+++ Itec/Pages/EventsManage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Itec.Pages
+{
+    public partial class EventsManage : Form
+    {
+        public EventsManage()
+        {
+            InitializeComponent();
+        }
+
+        private void iconButton4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
