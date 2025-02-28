eac48aac3057a84e551eccc4191c03a274fff150
Shahzaib Ali <163044429+stellalphatic@users.noreply.github.com>
2025-02-28 21:20:35 +0500

    changed theme of Login page

--- Itec/Pages/LoginPage.Designer.cs
+++ Itec/Pages/LoginPage.Designer.cs
@@ -32,23 +32,24 @@
             pictureBox1 = new PictureBox();
             label1 = new Label();
             label2 = new Label();
-            button1 = new Button();
+            SignupBtn = new Button();
             label3 = new Label();
             label4 = new Label();
+            lblToast = new Label();
             label5 = new Label();
-            panel1 = new Panel();
-            LoginBtn = new FontAwesome.Sharp.IconButton();
-            LoginPassword = new MaskedTextBox();
-            label7 = new Label();
             LoginUsername = new MaskedTextBox();
+            label7 = new Label();
+            LoginPassword = new MaskedTextBox();
+            LoginBtn = new FontAwesome.Sharp.IconButton();
             iconButton1 = new FontAwesome.Sharp.IconButton();
+            panel1 = new Panel();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             panel1.SuspendLayout();
             SuspendLayout();
             // 
             // pictureBox1
             // 
-            pictureBox1.BackgroundImage = Properties.Resources.purple1;
+            pictureBox1.BackColor = SystemColors.ActiveCaptionText;
             pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
             pictureBox1.Dock = DockStyle.Left;
             pictureBox1.InitialImage = null;
@@ -63,7 +64,6 @@
             label1.AutoSize = true;
             label1.BackColor = SystemColors.ActiveCaptionText;
             label1.Font = new Font("Impact", 21.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.Image = Properties.Resources.purple1;
             label1.Location = new Point(57, 91);
             label1.Name = "label1";
             label1.Size = new Size(142, 36);
@@ -75,7 +75,6 @@
             label2.AutoSize = true;
             label2.BackColor = SystemColors.ActiveCaptionText;
             label2.Font = new Font("Arial Narrow", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.Image = Properties.Resources.purple1;
             label2.Location = new Point(33, 162);
             label2.MaximumSize = new Size(200, 50);
             label2.Name = "label2";
@@ -83,18 +82,19 @@
             label2.TabIndex = 5;
             label2.Text = "Signup below to use the Application";
             // 
-            // button1
+            // SignupBtn
             // 
-            button1.AutoEllipsis = true;
-            button1.BackgroundImage = Properties.Resources.purple1;
-            button1.Cursor = Cursors.Hand;
-            button1.Font = new Font("Arial Rounded MT Bold", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button1.Location = new Point(70, 236);
-            button1.Name = "button1";
-            button1.Size = new Size(97, 33);
-            button1.TabIndex = 6;
-            button1.Text = "Signup";
-            button1.UseVisualStyleBackColor = true;
+            SignupBtn.AutoEllipsis = true;
+            SignupBtn.BackColor = SystemColors.ActiveCaptionText;
+            SignupBtn.Cursor = Cursors.Hand;
+            SignupBtn.Font = new Font("Arial Rounded MT Bold", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            SignupBtn.Location = new Point(70, 236);
+            SignupBtn.Name = "SignupBtn";
+            SignupBtn.Size = new Size(97, 33);
+            SignupBtn.TabIndex = 6;
+            SignupBtn.Text = "Signup";
+            SignupBtn.UseVisualStyleBackColor = false;
+            SignupBtn.Click += SignupBtn_Click;
             // 
             // label3
             // 
@@ -118,6 +118,16 @@
             label4.TabIndex = 8;
             label4.Text = "Event  Management  System";
             // 
+            // lblToast
+            // 
+            lblToast.BackColor = SystemColors.ActiveCaptionText;
+            lblToast.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblToast.Location = new Point(501, 127);
+            lblToast.Name = "lblToast";
+            lblToast.Size = new Size(100, 23);
+            lblToast.TabIndex = 11;
+            lblToast.Visible = false;
+            // 
             // label5
             // 
             label5.AutoSize = true;
@@ -130,20 +140,33 @@
             label5.Text = "Username:";
             label5.Click += label5_Click;
             // 
-            // panel1
+            // LoginUsername
             // 
-            panel1.BackgroundImage = Properties.Resources.mysticpurple;
-            panel1.BackgroundImageLayout = ImageLayout.Stretch;
-            panel1.Controls.Add(iconButton1);
-            panel1.Controls.Add(LoginBtn);
-            panel1.Controls.Add(LoginPassword);
-            panel1.Controls.Add(label7);
-            panel1.Controls.Add(LoginUsername);
-            panel1.Controls.Add(label5);
-            panel1.Location = new Point(382, 153);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(362, 266);
-            panel1.TabIndex = 10;
+            LoginUsername.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            LoginUsername.Location = new Point(155, 50);
+            LoginUsername.Name = "LoginUsername";
+            LoginUsername.Size = new Size(147, 27);
+            LoginUsername.TabIndex = 10;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = SystemColors.ActiveCaptionText;
+            label7.Font = new Font("Impact", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label7.Location = new Point(13, 103);
+            label7.Name = "label7";
+            label7.Size = new Size(101, 26);
+            label7.TabIndex = 12;
+            label7.Text = "Password:";
+            // 
+            // LoginPassword
+            // 
+            LoginPassword.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            LoginPassword.Location = new Point(155, 106);
+            LoginPassword.Name = "LoginPassword";
+            LoginPassword.Size = new Size(147, 27);
+            LoginPassword.TabIndex = 14;
+            LoginPassword.UseSystemPasswordChar = true;
             // 
             // LoginBtn
             // 
@@ -161,34 +184,6 @@
             LoginBtn.UseVisualStyleBackColor = false;
             LoginBtn.Click += LoginBtn_Click;
             // 
-            // LoginPassword
-            // 
-            LoginPassword.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            LoginPassword.Location = new Point(155, 106);
-            LoginPassword.Name = "LoginPassword";
-            LoginPassword.Size = new Size(147, 27);
-            LoginPassword.TabIndex = 14;
-            LoginPassword.UseSystemPasswordChar = true;
-            // 
-            // label7
-            // 
-            label7.AutoSize = true;
-            label7.BackColor = SystemColors.ActiveCaptionText;
-            label7.Font = new Font("Impact", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label7.Location = new Point(13, 103);
-            label7.Name = "label7";
-            label7.Size = new Size(101, 26);
-            label7.TabIndex = 12;
-            label7.Text = "Password:";
-            // 
-            // LoginUsername
-            // 
-            LoginUsername.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            LoginUsername.Location = new Point(155, 50);
-            LoginUsername.Name = "LoginUsername";
-            LoginUsername.Size = new Size(147, 27);
-            LoginUsername.TabIndex = 10;
-            // 
             // iconButton1
             // 
             iconButton1.IconChar = FontAwesome.Sharp.IconChar.Eye;
@@ -203,6 +198,21 @@
             iconButton1.UseVisualStyleBackColor = true;
             iconButton1.Click += iconButton1_Click;
             // 
+            // panel1
+            // 
+            panel1.BackgroundImage = Properties.Resources.mysticpurple;
+            panel1.BackgroundImageLayout = ImageLayout.Stretch;
+            panel1.Controls.Add(iconButton1);
+            panel1.Controls.Add(LoginBtn);
+            panel1.Controls.Add(LoginPassword);
+            panel1.Controls.Add(label7);
+            panel1.Controls.Add(LoginUsername);
+            panel1.Controls.Add(label5);
+            panel1.Location = new Point(382, 153);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(362, 266);
+            panel1.TabIndex = 10;
+            // 
             // LoginPage
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
@@ -210,10 +220,11 @@
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
             BackgroundImageLayout = ImageLayout.Center;
             ClientSize = new Size(839, 467);
+            Controls.Add(lblToast);
             Controls.Add(panel1);
             Controls.Add(label4);
             Controls.Add(label3);
-            Controls.Add(button1);
+            Controls.Add(SignupBtn);
             Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(pictureBox1);
@@ -234,15 +245,16 @@
         private PictureBox pictureBox1;
         private Label label1;
         private Label label2;
-        private Button button1;
+        private Button SignupBtn;
         private Label label3;
         private Label label4;
+        private Label lblToast;
         private Label label5;
-        private Panel panel1;
-        private MaskedTextBox LoginPassword;
-        private Label label7;
         private MaskedTextBox LoginUsername;
+        private Label label7;
+        private MaskedTextBox LoginPassword;
         private FontAwesome.Sharp.IconButton LoginBtn;
         private FontAwesome.Sharp.IconButton iconButton1;
+        private Panel panel1;
     }
 }
--- Itec/Pages/LoginPage.cs
+++ Itec/Pages/LoginPage.cs
@@ -1,3 +1,5 @@
+using System.Drawing.Drawing2D;
+
 namespace Itec
 {
     public partial class LoginPage : Form
@@ -21,21 +23,51 @@ namespace Itec
         {
 
         }
+        //private void LoginBtn_Paint(object sender, PaintEventArgs e)
+        //{
+        //    Button btn = (Button)sender;
+        //    GraphicsPath path = new GraphicsPath();
+        //    path.AddEllipse(0, 0, btn.Width, btn.Height);
+        //    btn.Region = new Region(path);
 
+        //}
+        public async void ShowToast(string message)
+        {
+            lblToast.Text = message;
+            lblToast.Visible = true;
+
+            await Task.Delay(3000); // Wait for 3 seconds
+            lblToast.Visible = false;
+        }
+
+        
         private void LoginBtn_Click(object sender, EventArgs e)
         {
             string username = LoginUsername.Text;
             string password = LoginPassword.Text;
 
+            ShowToast("Saved Successfully!");
+
+
         }
+      
 
         private void iconButton1_Click(object sender, EventArgs e)
         {
             var c = LoginPassword.UseSystemPasswordChar;
-            if (c==true)
+            if (c == true)
                 LoginPassword.UseSystemPasswordChar = false;
             else
                 LoginPassword.UseSystemPasswordChar = true;
+        }
+
+        private void SignupBtn_Click(object sender, EventArgs e)
+        {
+            SignupPage signupPage = new SignupPage();
+
+            signupPage.Show();
+            this.Hide();
+
         }
     }
 }
