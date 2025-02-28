2a35472ccdf00a7dbfa99eca3c4bf5de558e35ec
Shahzaib Ali <163044429+stellalphatic@users.noreply.github.com>
2025-02-28 21:25:28 +0500

    Added navigation in Venues Page

---
+++ Itec/Pages/Venues.Designer.cs
@@ -0,0 +1,343 @@
+﻿namespace Itec.Pages
+{
+    partial class Venues
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            panel1 = new Panel();
+            label1 = new Label();
+            sidePanel = new Panel();
+            iconButton1 = new FontAwesome.Sharp.IconButton();
+            iconButton4 = new FontAwesome.Sharp.IconButton();
+            reportBtn = new FontAwesome.Sharp.IconButton();
+            iconButton3 = new FontAwesome.Sharp.IconButton();
+            EditionsBtn = new FontAwesome.Sharp.IconButton();
+            venuesBtn = new FontAwesome.Sharp.IconButton();
+            ParticipantsBtn = new FontAwesome.Sharp.IconButton();
+            EventBtn = new FontAwesome.Sharp.IconButton();
+            commiteesBtn = new FontAwesome.Sharp.IconButton();
+            iconButton2 = new FontAwesome.Sharp.IconButton();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            panel1.SuspendLayout();
+            sidePanel.SuspendLayout();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(326, 55);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(543, 303);
+            dataGridView1.TabIndex = 7;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.Highlight;
+            panel1.Controls.Add(label1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(161, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(716, 49);
+            panel1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Gill Sans Ultra Bold", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ButtonHighlight;
+            label1.Location = new Point(394, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(89, 39);
+            label1.TabIndex = 2;
+            label1.Text = "ITEC";
+            // 
+            // sidePanel
+            // 
+            sidePanel.BackColor = SystemColors.HotTrack;
+            sidePanel.Controls.Add(iconButton1);
+            sidePanel.Controls.Add(iconButton4);
+            sidePanel.Controls.Add(reportBtn);
+            sidePanel.Controls.Add(iconButton3);
+            sidePanel.Controls.Add(EditionsBtn);
+            sidePanel.Controls.Add(venuesBtn);
+            sidePanel.Controls.Add(ParticipantsBtn);
+            sidePanel.Controls.Add(EventBtn);
+            sidePanel.Controls.Add(commiteesBtn);
+            sidePanel.Controls.Add(iconButton2);
+            sidePanel.Dock = DockStyle.Left;
+            sidePanel.ForeColor = SystemColors.ButtonHighlight;
+            sidePanel.Location = new Point(0, 0);
+            sidePanel.Name = "sidePanel";
+            sidePanel.Size = new Size(161, 548);
+            sidePanel.TabIndex = 6;
+            // 
+            // iconButton1
+            // 
+            iconButton1.BackColor = SystemColors.MenuHighlight;
+            iconButton1.FlatAppearance.BorderColor = SystemColors.ActiveCaptionText;
+            iconButton1.FlatAppearance.BorderSize = 2;
+            iconButton1.FlatAppearance.CheckedBackColor = Color.Black;
+            iconButton1.FlatAppearance.MouseDownBackColor = SystemColors.ActiveCaptionText;
+            iconButton1.FlatAppearance.MouseOverBackColor = SystemColors.AppWorkspace;
+            iconButton1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            iconButton1.IconChar = FontAwesome.Sharp.IconChar.FileText;
+            iconButton1.IconColor = Color.White;
+            iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton1.IconSize = 30;
+            iconButton1.ImageAlign = ContentAlignment.MiddleLeft;
+            iconButton1.Location = new Point(0, 325);
+            iconButton1.Name = "iconButton1";
+            iconButton1.Size = new Size(158, 33);
+            iconButton1.TabIndex = 8;
+            iconButton1.Text = "Duties";
+            iconButton1.UseVisualStyleBackColor = false;
+            // 
+            // iconButton4
+            // 
+            iconButton4.BackColor = SystemColors.MenuHighlight;
+            iconButton4.FlatAppearance.BorderColor = SystemColors.ActiveCaptionText;
+            iconButton4.FlatAppearance.BorderSize = 2;
+            iconButton4.FlatAppearance.CheckedBackColor = Color.Black;
+            iconButton4.FlatAppearance.MouseDownBackColor = SystemColors.ActiveCaptionText;
+            iconButton4.FlatAppearance.MouseOverBackColor = SystemColors.AppWorkspace;
+            iconButton4.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            iconButton4.IconChar = FontAwesome.Sharp.IconChar.CediSign;
+            iconButton4.IconColor = Color.White;
+            iconButton4.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton4.IconSize = 30;
+            iconButton4.ImageAlign = ContentAlignment.MiddleLeft;
+            iconButton4.Location = new Point(0, 498);
+            iconButton4.Name = "iconButton4";
+            iconButton4.Size = new Size(161, 33);
+            iconButton4.TabIndex = 8;
+            iconButton4.Text = "Logout";
+            iconButton4.UseVisualStyleBackColor = false;
+            // 
+            // reportBtn
+            // 
+            reportBtn.BackColor = SystemColors.MenuHighlight;
+            reportBtn.FlatAppearance.BorderColor = SystemColors.ActiveCaptionText;
+            reportBtn.FlatAppearance.BorderSize = 2;
+            reportBtn.FlatAppearance.CheckedBackColor = Color.Black;
+            reportBtn.FlatAppearance.MouseDownBackColor = SystemColors.ActiveCaptionText;
+            reportBtn.FlatAppearance.MouseOverBackColor = SystemColors.AppWorkspace;
+            reportBtn.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            reportBtn.IconChar = FontAwesome.Sharp.IconChar.FileText;
+            reportBtn.IconColor = Color.White;
+            reportBtn.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            reportBtn.IconSize = 30;
+            reportBtn.ImageAlign = ContentAlignment.MiddleLeft;
+            reportBtn.Location = new Point(0, 403);
+            reportBtn.Name = "reportBtn";
+            reportBtn.Size = new Size(158, 33);
+            reportBtn.TabIndex = 7;
+            reportBtn.Text = "Report";
+            reportBtn.UseVisualStyleBackColor = false;
+            // 
+            // iconButton3
+            // 
+            iconButton3.BackColor = SystemColors.MenuHighlight;
+            iconButton3.FlatAppearance.BorderColor = SystemColors.ActiveCaptionText;
+            iconButton3.FlatAppearance.BorderSize = 2;
+            iconButton3.FlatAppearance.CheckedBackColor = Color.Black;
+            iconButton3.FlatAppearance.MouseDownBackColor = SystemColors.ActiveCaptionText;
+            iconButton3.FlatAppearance.MouseOverBackColor = SystemColors.AppWorkspace;
+            iconButton3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            iconButton3.IconChar = FontAwesome.Sharp.IconChar.Donate;
+            iconButton3.IconColor = Color.White;
+            iconButton3.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton3.IconSize = 30;
+            iconButton3.ImageAlign = ContentAlignment.MiddleLeft;
+            iconButton3.Location = new Point(0, 286);
+            iconButton3.Name = "iconButton3";
+            iconButton3.Size = new Size(158, 33);
+            iconButton3.TabIndex = 6;
+            iconButton3.Text = "Finances";
+            iconButton3.UseVisualStyleBackColor = false;
+            // 
+            // EditionsBtn
+            // 
+            EditionsBtn.BackColor = SystemColors.MenuHighlight;
+            EditionsBtn.FlatAppearance.BorderColor = SystemColors.ActiveCaptionText;
+            EditionsBtn.FlatAppearance.BorderSize = 2;
+            EditionsBtn.FlatAppearance.CheckedBackColor = Color.Black;
+            EditionsBtn.FlatAppearance.MouseDownBackColor = SystemColors.ActiveCaptionText;
+            EditionsBtn.FlatAppearance.MouseOverBackColor = SystemColors.AppWorkspace;
+            EditionsBtn.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            EditionsBtn.IconChar = FontAwesome.Sharp.IconChar.Mizuni;
+            EditionsBtn.IconColor = Color.White;
+            EditionsBtn.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            EditionsBtn.IconSize = 30;
+            EditionsBtn.ImageAlign = ContentAlignment.MiddleLeft;
+            EditionsBtn.Location = new Point(0, 364);
+            EditionsBtn.Name = "EditionsBtn";
+            EditionsBtn.Size = new Size(158, 33);
+            EditionsBtn.TabIndex = 5;
+            EditionsBtn.Text = "Editions";
+            EditionsBtn.UseVisualStyleBackColor = false;
+            // 
+            // venuesBtn
+            // 
+            venuesBtn.BackColor = SystemColors.MenuHighlight;
+            venuesBtn.FlatAppearance.BorderColor = SystemColors.ActiveCaptionText;
+            venuesBtn.FlatAppearance.BorderSize = 2;
+            venuesBtn.FlatAppearance.CheckedBackColor = Color.Black;
+            venuesBtn.FlatAppearance.MouseDownBackColor = SystemColors.ActiveCaptionText;
+            venuesBtn.FlatAppearance.MouseOverBackColor = SystemColors.AppWorkspace;
+            venuesBtn.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            venuesBtn.IconChar = FontAwesome.Sharp.IconChar.Building;
+            venuesBtn.IconColor = Color.White;
+            venuesBtn.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            venuesBtn.IconSize = 30;
+            venuesBtn.ImageAlign = ContentAlignment.MiddleLeft;
+            venuesBtn.Location = new Point(0, 208);
+            venuesBtn.Name = "venuesBtn";
+            venuesBtn.Size = new Size(158, 33);
+            venuesBtn.TabIndex = 4;
+            venuesBtn.Text = "Venues";
+            venuesBtn.UseVisualStyleBackColor = false;
+            // 
+            // ParticipantsBtn
+            // 
+            ParticipantsBtn.BackColor = SystemColors.MenuHighlight;
+            ParticipantsBtn.FlatAppearance.BorderColor = SystemColors.ActiveCaptionText;
+            ParticipantsBtn.FlatAppearance.BorderSize = 2;
+            ParticipantsBtn.FlatAppearance.CheckedBackColor = Color.Black;
+            ParticipantsBtn.FlatAppearance.MouseDownBackColor = SystemColors.ActiveCaptionText;
+            ParticipantsBtn.FlatAppearance.MouseOverBackColor = SystemColors.AppWorkspace;
+            ParticipantsBtn.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            ParticipantsBtn.IconChar = FontAwesome.Sharp.IconChar.Users;
+            ParticipantsBtn.IconColor = Color.White;
+            ParticipantsBtn.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            ParticipantsBtn.IconSize = 32;
+            ParticipantsBtn.ImageAlign = ContentAlignment.MiddleLeft;
+            ParticipantsBtn.Location = new Point(0, 169);
+            ParticipantsBtn.Name = "ParticipantsBtn";
+            ParticipantsBtn.Size = new Size(158, 33);
+            ParticipantsBtn.TabIndex = 3;
+            ParticipantsBtn.Text = "Participants";
+            ParticipantsBtn.UseVisualStyleBackColor = false;
+            // 
+            // EventBtn
+            // 
+            EventBtn.BackColor = SystemColors.MenuHighlight;
+            EventBtn.FlatAppearance.BorderColor = SystemColors.ActiveCaptionText;
+            EventBtn.FlatAppearance.BorderSize = 2;
+            EventBtn.FlatAppearance.CheckedBackColor = Color.Black;
+            EventBtn.FlatAppearance.MouseDownBackColor = SystemColors.ActiveCaptionText;
+            EventBtn.FlatAppearance.MouseOverBackColor = SystemColors.AppWorkspace;
+            EventBtn.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            EventBtn.IconChar = FontAwesome.Sharp.IconChar.DiceD20;
+            EventBtn.IconColor = Color.White;
+            EventBtn.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            EventBtn.IconSize = 30;
+            EventBtn.ImageAlign = ContentAlignment.MiddleLeft;
+            EventBtn.Location = new Point(0, 130);
+            EventBtn.Name = "EventBtn";
+            EventBtn.Size = new Size(158, 33);
+            EventBtn.TabIndex = 2;
+            EventBtn.Text = "Events";
+            EventBtn.UseVisualStyleBackColor = false;
+            // 
+            // commiteesBtn
+            // 
+            commiteesBtn.BackColor = SystemColors.MenuHighlight;
+            commiteesBtn.FlatAppearance.BorderColor = SystemColors.ActiveCaptionText;
+            commiteesBtn.FlatAppearance.BorderSize = 2;
+            commiteesBtn.FlatAppearance.CheckedBackColor = Color.Black;
+            commiteesBtn.FlatAppearance.MouseDownBackColor = SystemColors.ActiveCaptionText;
+            commiteesBtn.FlatAppearance.MouseOverBackColor = SystemColors.AppWorkspace;
+            commiteesBtn.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            commiteesBtn.IconChar = FontAwesome.Sharp.IconChar.Connectdevelop;
+            commiteesBtn.IconColor = Color.White;
+            commiteesBtn.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            commiteesBtn.IconSize = 35;
+            commiteesBtn.ImageAlign = ContentAlignment.MiddleLeft;
+            commiteesBtn.Location = new Point(0, 247);
+            commiteesBtn.Name = "commiteesBtn";
+            commiteesBtn.Size = new Size(158, 33);
+            commiteesBtn.TabIndex = 1;
+            commiteesBtn.Text = "Commitees";
+            commiteesBtn.UseVisualStyleBackColor = false;
+            // 
+            // iconButton2
+            // 
+            iconButton2.BackColor = SystemColors.MenuHighlight;
+            iconButton2.FlatAppearance.BorderColor = SystemColors.ActiveCaptionText;
+            iconButton2.FlatAppearance.BorderSize = 2;
+            iconButton2.FlatAppearance.CheckedBackColor = Color.Black;
+            iconButton2.FlatAppearance.MouseDownBackColor = SystemColors.ActiveCaptionText;
+            iconButton2.FlatAppearance.MouseOverBackColor = SystemColors.AppWorkspace;
+            iconButton2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            iconButton2.IconChar = FontAwesome.Sharp.IconChar.Modx;
+            iconButton2.IconColor = Color.White;
+            iconButton2.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton2.IconSize = 30;
+            iconButton2.ImageAlign = ContentAlignment.MiddleLeft;
+            iconButton2.Location = new Point(0, 91);
+            iconButton2.Name = "iconButton2";
+            iconButton2.Size = new Size(158, 33);
+            iconButton2.TabIndex = 0;
+            iconButton2.Text = "Dashboard";
+            iconButton2.UseVisualStyleBackColor = false;
+            // 
+            // Venues
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(877, 548);
+            Controls.Add(dataGridView1);
+            Controls.Add(panel1);
+            Controls.Add(sidePanel);
+            Name = "Venues";
+            Text = "Venues";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            sidePanel.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Panel panel1;
+        private Label label1;
+        private Panel sidePanel;
+        private FontAwesome.Sharp.IconButton iconButton1;
+        private FontAwesome.Sharp.IconButton iconButton4;
+        private FontAwesome.Sharp.IconButton reportBtn;
+        private FontAwesome.Sharp.IconButton iconButton3;
+        private FontAwesome.Sharp.IconButton EditionsBtn;
+        private FontAwesome.Sharp.IconButton venuesBtn;
+        private FontAwesome.Sharp.IconButton ParticipantsBtn;
+        private FontAwesome.Sharp.IconButton EventBtn;
+        private FontAwesome.Sharp.IconButton commiteesBtn;
+        private FontAwesome.Sharp.IconButton iconButton2;
+    }
+}
