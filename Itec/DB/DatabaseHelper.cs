8aad8d0e58c39e534fce208a53e77f57f12bb4a3
Shahzaib Ali <163044429+stellalphatic@users.noreply.github.com>
2025-03-16 13:28:11 +0500

    Database Helper Code
    created two functions for getting data and Executing Queries using MySql.Data package

---
+++ Itec/DB/DatabaseHelper.cs
@@ -0,0 +1,40 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+ namespace Itec.DB
+{
+    public static class DatabaseHelper
+    {
+        private static string connectionString = "server=127.0.0.1;port=3307;user=root;database=itec;password=12345678900987654321;";
+
+        //For SELECT Queries
+        public static DataTable GetData(string query)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                {
+                    DataTable dt = new DataTable();
+                    new MySqlDataAdapter(cmd).Fill(dt);
+                    return dt;
+                }
+            }
+        }
+
+        //For INSERT,UPDATE,DELETE Queries
+        public static void ExecuteQuery(string query)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
