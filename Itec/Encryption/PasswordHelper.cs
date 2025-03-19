80272bd5c4c49b5d511739513a8c7086be228e57
Shahzaib Ali <163044429+stellalphatic@users.noreply.github.com>
2025-03-19 15:38:58 +0500

    Password Helper class for hashing and verification
    I initially tried to make RSA encryption algorithm but due than implemented hashing through bcrypt, to use this first you need to install BCrypt.Net-Next package

---
+++ Itec/Encryption/PasswordHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Itec.Encryption
+{
+    public static class PasswordHelper
+    {
+        // Hash a password
+        public static string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password, BCrypt.Net.BCrypt.GenerateSalt(12));
+        }
+
+        // Verify a password against a hash
+        public static bool VerifyPassword(string password, string hash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hash);
+        }
+    }
+}
