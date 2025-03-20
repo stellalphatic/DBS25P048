c92a5ad5fbb2b17e420f94537d62397da6c31316
Shahzaib Ali <163044429+stellalphatic@users.noreply.github.com>
2025-03-20 23:07:17 +0500

    Updated EventParticipants
    added some reference attributes for display fields of user

---
+++ Itec/BL/EventParticipantBL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Itec.BL
+{
+    public class EventParticipant
+    {
+        public int RegistrationId { get; set; }
+        public int EventId { get; set; }
+        public int ParticipantId { get; set; }
+        public int PaymentStatusId { get; set; }
+        public decimal FeeAmount { get; set; }
+
+        //Display fields
+        public string EventName { get; set; }  //Ref
+        public string ParticipantName { get; set; } //Ref
+        public string PaymentStatus { get; set; } //Ref
+    }
+}
