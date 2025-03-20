e5d4bcad65fae0658b8ef24adb6df999b2eb9e46
Shahzaib Ali <163044429+stellalphatic@users.noreply.github.com>
2025-03-20 23:00:07 +0500

    Added members to CommitteeDL
    added  committee members DL class to same file with already existing CommitteeDL class also changes this class by adding refrence values

--- Itec/DL/CommitteeDL.cs
+++ Itec/DL/CommitteeDL.cs
@@ -12,11 +12,12 @@ namespace Itec.DL
 {
     public static class CommitteeDL
     {
-        //Get All Committtee
+        //Get All Committtees
         public static List<Committee> GetAllCommittees()
         {
             List<Committee> committees = new List<Committee>();
-            string query = "SELECT * FROM committees";
+            string query = "SELECT c.committee_id,c.itec_id,ie.year,c.committee_name FROM committees c" +
+                " LEFT JOIN itec_editions ie on ie.itec_id=c.itec_id";
             DataTable dt = DatabaseHelper.GetData(query);
 
             foreach (DataRow row in dt.Rows)
@@ -25,6 +26,7 @@ namespace Itec.DL
                 {
                     CommitteeId = Convert.ToInt32(row["committee_id"]),
                     ItecId = row["itec_id"] != DBNull.Value ? Convert.ToInt32(row["itec_id"]) : (int?)null,
+                    Year = Convert.ToInt32(row["year"]),
                     CommitteeName = row["committee_name"].ToString()
                 });
             }
@@ -54,6 +56,62 @@ namespace Itec.DL
         public static void DeleteCommittee(int committeeId)
         {
             string query = $"DELETE FROM committees WHERE committee_id = {committeeId}";
+            DatabaseHelper.ExecuteQuery(query);
+        }
+    }
+    public static class CommitteeMemberDL
+    {
+        public static List<CommitteeMember> GetAllCommitteeMembers()
+        {
+            List<CommitteeMember> members = new List<CommitteeMember>();
+            string query = @"
+            SELECT cm.member_id, cm.committee_id, cm.name, cm.role_id,c.committee_name, l.value AS role_name
+            FROM committee_members cm
+            LEFT JOIN committees c on cm.committee_id = c.committee_id
+            LEFT JOIN lookup l on cm.role_id = l.lookup_id";
+
+            DataTable dt = DatabaseHelper.GetData(query);
+
+            foreach (DataRow row in dt.Rows)
+            {
+                members.Add(new CommitteeMember
+                {
+                    MemberId = row["member_id"] == DBNull.Value ? 0 : Convert.ToInt32(row["member_id"]),
+                    CommitteeId = row["committee_id"] == DBNull.Value ? 0 : Convert.ToInt32(row["committee_id"]),
+                    Name = row["name"].ToString(),
+                    RoleId = row["role_id"] == DBNull.Value ? (int?)null : Convert.ToInt32(row["role_id"]),
+                    CommitteeName = row["committee_name"].ToString(),
+                    RoleName = row["role_name"].ToString()
+                });
+            }
+            return members;
+        }
+
+        public static void AddCommitteeMember(CommitteeMember member)
+        {
+            string query = $@"
+            INSERT INTO committee_members (committee_id, name, role_id)
+            VALUES ({member.CommitteeId}, '{member.Name}', 
+                    {(member.RoleId.HasValue ? member.RoleId.ToString() : "NULL")})";
+
+            DatabaseHelper.ExecuteQuery(query);
+        }
+
+        public static void UpdateCommitteeMember(CommitteeMember member)
+        {
+            string query = $@"
+            UPDATE committee_members 
+            SET committee_id = {member.CommitteeId},
+                name = '{member.Name}',
+                role_id = {(member.RoleId.HasValue ? member.RoleId.ToString() : "NULL")}
+            WHERE member_id = {member.MemberId}";
+
+            DatabaseHelper.ExecuteQuery(query);
+        }
+
+        public static void DeleteCommitteeMember(int memberId)
+        {
+            string query = $"DELETE FROM committee_members WHERE member_id = {memberId}";
             DatabaseHelper.ExecuteQuery(query);
         }
     }
