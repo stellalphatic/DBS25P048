8e65ac1334af9d206d9c48cd1258dbeff9f7bca7
Shahzaib Ali <163044429+stellalphatic@users.noreply.github.com>
2025-03-20 23:14:00 +0500

    Created EventResult BL Class
    Created event Results class for managing the results of all participants in Events

---
+++ Itec/BL/EventResultBL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Itec.BL
+{
+    public class EventResult
+    {
+        public int ResultId { get; set; }
+        public int EventId { get; set; }
+        public int? ParticipantId { get; set; }
+        public int? TeamId { get; set; }
+        public int Position { get; set; }
+        public decimal Score { get; set; }
+        public string Remarks { get; set; }
+
+        // Display fields
+        public string EventName { get; set; }   //Ref
+        public string ParticipantName { get; set; } //Ref
+        public string TeamName { get; set; }   //Ref
+    }
+}
