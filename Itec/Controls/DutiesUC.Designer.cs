a768244fea9bdf351a935a14276ad839cbbc75d6
Shahzaib Ali <163044429+stellalphatic@users.noreply.github.com>
2025-03-21 23:21:01 +0500

    Updated Duties page
    resolved some errors and populated IDs

--- Itec/Controls/DutiesUC.Designer.cs
+++ Itec/Controls/DutiesUC.Designer.cs
@@ -31,11 +31,28 @@
             panel1 = new Panel();
             label2 = new Label();
             MainPanel = new Panel();
-            button4 = new Button();
+            txtAssignedTo = new TextBox();
+            label5 = new Label();
+            dtpDeadline = new DateTimePicker();
+            TaskDescriptiontxt = new RichTextBox();
+            cmbStatus = new ComboBox();
+            cmbCommittee = new ComboBox();
+            label12 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label1 = new Label();
             button3 = new Button();
             button2 = new Button();
             button1 = new Button();
             dataGridView1 = new DataGridView();
+            DutyId = new DataGridViewTextBoxColumn();
+            CommitteeID = new DataGridViewTextBoxColumn();
+            CommitteeName = new DataGridViewTextBoxColumn();
+            AssignedTo = new DataGridViewTextBoxColumn();
+            TaskDescription = new DataGridViewTextBoxColumn();
+            Deadline = new DataGridViewTextBoxColumn();
+            StatusId = new DataGridViewTextBoxColumn();
+            StatusName = new DataGridViewTextBoxColumn();
             panel1.SuspendLayout();
             MainPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
@@ -65,7 +82,16 @@
             // MainPanel
             // 
             MainPanel.BackColor = SystemColors.ButtonFace;
-            MainPanel.Controls.Add(button4);
+            MainPanel.Controls.Add(txtAssignedTo);
+            MainPanel.Controls.Add(label5);
+            MainPanel.Controls.Add(dtpDeadline);
+            MainPanel.Controls.Add(TaskDescriptiontxt);
+            MainPanel.Controls.Add(cmbStatus);
+            MainPanel.Controls.Add(cmbCommittee);
+            MainPanel.Controls.Add(label12);
+            MainPanel.Controls.Add(label4);
+            MainPanel.Controls.Add(label3);
+            MainPanel.Controls.Add(label1);
             MainPanel.Controls.Add(button3);
             MainPanel.Controls.Add(button2);
             MainPanel.Controls.Add(button1);
@@ -76,57 +102,188 @@
             MainPanel.Size = new Size(982, 495);
             MainPanel.TabIndex = 11;
             // 
-            // button4
+            // txtAssignedTo
             // 
-            button4.BackColor = SystemColors.ActiveCaption;
-            button4.Font = new Font("Sitka Small", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button4.Location = new Point(584, 354);
-            button4.Name = "button4";
-            button4.Size = new Size(75, 31);
-            button4.TabIndex = 11;
-            button4.Text = "Search";
-            button4.UseVisualStyleBackColor = false;
+            txtAssignedTo.Location = new Point(174, 102);
+            txtAssignedTo.Name = "txtAssignedTo";
+            txtAssignedTo.Size = new Size(158, 23);
+            txtAssignedTo.TabIndex = 54;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Cursor = Cursors.Cross;
+            label5.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(44, 101);
+            label5.Name = "label5";
+            label5.Size = new Size(92, 20);
+            label5.TabIndex = 53;
+            label5.Text = "Assigned to:";
+            // 
+            // dtpDeadline
+            // 
+            dtpDeadline.CustomFormat = "yyyy-MM-dd";
+            dtpDeadline.Format = DateTimePickerFormat.Custom;
+            dtpDeadline.Location = new Point(174, 152);
+            dtpDeadline.Name = "dtpDeadline";
+            dtpDeadline.Size = new Size(158, 23);
+            dtpDeadline.TabIndex = 51;
+            dtpDeadline.ValueChanged += dtpDeadline_ValueChanged;
+            // 
+            // TaskDescriptiontxt
+            // 
+            TaskDescriptiontxt.Location = new Point(174, 240);
+            TaskDescriptiontxt.Name = "TaskDescriptiontxt";
+            TaskDescriptiontxt.Size = new Size(158, 55);
+            TaskDescriptiontxt.TabIndex = 50;
+            TaskDescriptiontxt.Text = "";
+            // 
+            // cmbStatus
+            // 
+            cmbStatus.FormattingEnabled = true;
+            cmbStatus.Items.AddRange(new object[] { "1", "2", "3" });
+            cmbStatus.Location = new Point(174, 196);
+            cmbStatus.Name = "cmbStatus";
+            cmbStatus.Size = new Size(158, 23);
+            cmbStatus.TabIndex = 49;
+            // 
+            // cmbCommittee
+            // 
+            cmbCommittee.FormattingEnabled = true;
+            cmbCommittee.Items.AddRange(new object[] { "1", "2", "3" });
+            cmbCommittee.Location = new Point(174, 49);
+            cmbCommittee.Name = "cmbCommittee";
+            cmbCommittee.Size = new Size(158, 23);
+            cmbCommittee.TabIndex = 48;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Cursor = Cursors.Cross;
+            label12.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label12.Location = new Point(44, 240);
+            label12.Name = "label12";
+            label12.Size = new Size(124, 20);
+            label12.TabIndex = 47;
+            label12.Text = "Task Description:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Cursor = Cursors.Cross;
+            label4.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(44, 199);
+            label4.Name = "label4";
+            label4.Size = new Size(54, 20);
+            label4.TabIndex = 46;
+            label4.Text = "Status:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Cursor = Cursors.Cross;
+            label3.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(44, 154);
+            label3.Name = "label3";
+            label3.Size = new Size(74, 20);
+            label3.TabIndex = 45;
+            label3.Text = "Deadline:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Cursor = Cursors.Cross;
+            label1.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(44, 49);
+            label1.Name = "label1";
+            label1.Size = new Size(87, 20);
+            label1.TabIndex = 44;
+            label1.Text = "Committee:";
             // 
             // button3
             // 
             button3.BackColor = SystemColors.ActiveCaption;
             button3.Font = new Font("Sitka Small", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button3.Location = new Point(450, 356);
+            button3.Location = new Point(290, 341);
             button3.Name = "button3";
             button3.Size = new Size(75, 31);
             button3.TabIndex = 8;
             button3.Text = "Update";
             button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
             // 
             // button2
             // 
             button2.BackColor = SystemColors.ActiveCaption;
             button2.Font = new Font("Sitka Small", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button2.Location = new Point(329, 354);
+            button2.Location = new Point(185, 362);
             button2.Name = "button2";
             button2.Size = new Size(75, 31);
             button2.TabIndex = 7;
             button2.Text = "Delete";
             button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
             // 
             // button1
             // 
             button1.BackColor = SystemColors.ActiveCaption;
             button1.Font = new Font("Sitka Small", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button1.Location = new Point(329, 300);
+            button1.Location = new Point(77, 326);
             button1.Name = "button1";
             button1.Size = new Size(75, 31);
             button1.TabIndex = 6;
             button1.Text = "Insert";
             button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
             // 
             // dataGridView1
             // 
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(462, 18);
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { DutyId, CommitteeID, CommitteeName, AssignedTo, TaskDescription, Deadline, StatusId, StatusName });
+            dataGridView1.Location = new Point(414, 18);
             dataGridView1.Name = "dataGridView1";
-            dataGridView1.Size = new Size(495, 291);
+            dataGridView1.Size = new Size(543, 291);
             dataGridView1.TabIndex = 5;
+            // 
+            // DutyId
+            // 
+            DutyId.HeaderText = "DutyId";
+            DutyId.Name = "DutyId";
+            // 
+            // CommitteeID
+            // 
+            CommitteeID.HeaderText = "CommitteeID";
+            CommitteeID.Name = "CommitteeID";
+            // 
+            // CommitteeName
+            // 
+            CommitteeName.HeaderText = "CommitteeName";
+            CommitteeName.Name = "CommitteeName";
+            // 
+            // AssignedTo
+            // 
+            AssignedTo.HeaderText = "Assigned_to";
+            AssignedTo.Name = "AssignedTo";
+            // 
+            // TaskDescription
+            // 
+            TaskDescription.HeaderText = "Task_Description";
+            TaskDescription.Name = "TaskDescription";
+            // 
+            // Deadline
+            // 
+            Deadline.HeaderText = "Deadline";
+            Deadline.Name = "Deadline";
+            // 
+            // StatusId
+            // 
+            StatusId.HeaderText = "StatusId";
+            StatusId.Name = "StatusId";
+            // 
+            // StatusName
+            // 
+            StatusName.HeaderText = "Status";
+            StatusName.Name = "StatusName";
             // 
             // DutiesUC
             // 
@@ -139,6 +296,7 @@
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
             MainPanel.ResumeLayout(false);
+            MainPanel.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
         }
@@ -148,10 +306,27 @@
         private Panel panel1;
         private Label label2;
         private Panel MainPanel;
-        private Button button4;
         private Button button3;
         private Button button2;
         private Button button1;
         private DataGridView dataGridView1;
+        private Label label12;
+        private Label label4;
+        private Label label3;
+        private Label label1;
+        private ComboBox cmbStatus;
+        private ComboBox cmbCommittee;
+        private RichTextBox TaskDescriptiontxt;
+        private DateTimePicker dtpDeadline;
+        private Label label5;
+        private TextBox txtAssignedTo;
+        private DataGridViewTextBoxColumn DutyId;
+        private DataGridViewTextBoxColumn CommitteeID;
+        private DataGridViewTextBoxColumn CommitteeName;
+        private DataGridViewTextBoxColumn AssignedTo;
+        private DataGridViewTextBoxColumn TaskDescription;
+        private DataGridViewTextBoxColumn Deadline;
+        private DataGridViewTextBoxColumn StatusId;
+        private DataGridViewTextBoxColumn StatusName;
     }
 }
--- Itec/Controls/DutiesUC.cs
+++ Itec/Controls/DutiesUC.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Itec.BL;
+using Itec.DB;
+using Itec.DL;
+using Org.BouncyCastle.Asn1.Cmp;
 
 namespace Itec.Controls
 {
@@ -15,6 +19,171 @@ namespace Itec.Controls
         public DutiesUC()
         {
             InitializeComponent();
+            LoadDuties();
+            PopulateComboboxes();
+
+        }
+
+        private void LoadDuties()
+        {
+            var duties = DutyDL.GetAllDuties();
+            dataGridView1.Rows.Clear();
+
+            foreach (var duty in duties)
+            {
+                dataGridView1.Rows.Add(
+                    duty.DutyId,
+                    duty.CommitteeId,
+                    duty.CommitteeName,
+                    duty.AssignedTo,
+                    duty.TaskDescription,
+                    duty.Deadline.ToString("yyyy-MM-dd"),
+                    duty.StatusId,
+                    duty.StatusName
+                );
+            }
+            dataGridView1.Columns["CommitteeID"].Visible = false;
+            dataGridView1.Columns["StatusId"].Visible = false;
+            dataGridView1.Columns["DutyId"].Visible = false;
+        }
+
+        private void PopulateComboboxes()
+        {
+            //Committtees
+            string committeeQuery = "SELECT committee_id, committee_name FROM committees";
+            DataTable committeeData = DatabaseHelper.GetData(committeeQuery);
+            cmbCommittee.DataSource = committeeData;
+            cmbCommittee.DisplayMember = "committee_name";
+            cmbCommittee.ValueMember = "committee_id";
+
+            //Statuses
+            string statusQuery = "SELECT lookup_id, value FROM lookup WHERE category = 'DutyStatus'";
+            DataTable statusData = DatabaseHelper.GetData(statusQuery);
+            cmbStatus.DataSource = statusData;
+            cmbStatus.DisplayMember = "value";
+            cmbStatus.ValueMember = "lookup_id";
+        }
+
+
+
+
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                var selectedRow = dataGridView1.SelectedRows[0];
+
+
+                cmbCommittee.SelectedValue = selectedRow.Cells["CommitteeId"].Value;
+                TaskDescriptiontxt.Text = selectedRow.Cells["TaskDescription"].Value.ToString();
+                dtpDeadline.Value = Convert.ToDateTime(selectedRow.Cells["Deadline"].Value);
+                cmbStatus.SelectedValue = selectedRow.Cells["StatusId"].Value;
+                txtAssignedTo.Text = selectedRow.Cells["AssignedTo"].Value.ToString();
+
+            }
+        }
+
+
+        //Insert Button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (cmbCommittee.SelectedValue == null || string.IsNullOrEmpty(txtAssignedTo.Text))
+            {
+                MessageBox.Show("Please select a committee and enter an assignee");
+                return;
+            }
+
+            var newDuty = new Duty
+            {
+                CommitteeId = Convert.ToInt32(cmbCommittee.SelectedValue),
+                AssignedTo = txtAssignedTo.Text,
+                TaskDescription = TaskDescriptiontxt.Text,
+                Deadline = dtpDeadline.Value,
+                StatusId = cmbStatus.SelectedValue != null ? Convert.ToInt32(cmbStatus.SelectedValue) : 0
+            };
+
+            try
+            {
+                DutyDL.AddDuty(newDuty);
+                LoadDuties();
+                MessageBox.Show("Duty added successfully!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error adding duty: {ex.Message}");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Please select a duty to update.");
+                return;
+            }
+            else
+            {
+                var selectedRow = dataGridView1.SelectedRows[0];
+
+                //Input fields
+                cmbCommittee.SelectedValue = selectedRow.Cells["CommitteeID"].Value;
+                txtAssignedTo.Text = selectedRow.Cells["AssignedTo"].Value.ToString();
+                TaskDescriptiontxt.Text = selectedRow.Cells["TaskDescription"].Value.ToString();
+                dtpDeadline.Value = Convert.ToDateTime(selectedRow.Cells["Deadline"].Value);
+                cmbStatus.SelectedValue = selectedRow.Cells["StatusId"].Value;
+
+            }
+
+            int dutyId = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["DutyId"].Value);
+
+            var updatedDuty = new Duty
+            {
+                DutyId = dutyId,
+                CommitteeId = Convert.ToInt32(cmbCommittee.SelectedValue),
+                AssignedTo = txtAssignedTo.Text,
+                TaskDescription = TaskDescriptiontxt.Text,
+                Deadline = dtpDeadline.Value,
+                StatusId = cmbStatus.SelectedValue != null ? Convert.ToInt32(cmbStatus.SelectedValue) : 0
+            };
+
+            try
+            {
+                DutyDL.UpdateDuty(updatedDuty);
+                LoadDuties();
+                MessageBox.Show("Duty updated successfully!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error updating duty: {ex.Message}");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Please select a duty to delete");
+                return;
+            }
+
+            int dutyId = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["DutyId"].Value);
+
+            try
+            {
+                DutyDL.DeleteDuty(dutyId);
+                LoadDuties();
+                MessageBox.Show("Duty deleted successfully");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error deleting duty: {ex.Message}");
+            }
+        }
+
+        private void dtpDeadline_ValueChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
