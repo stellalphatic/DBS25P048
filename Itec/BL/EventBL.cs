d6becb5497d16f443f049892f69d5ff0a7475ff7
Shahzaib Ali <163044429+stellalphatic@users.noreply.github.com>
2025-03-23 05:42:18 +0500

    updated event class

--- Itec/BL/EventBL.cs
+++ Itec/BL/EventBL.cs
@@ -8,12 +8,19 @@ namespace Itec.BL
 {
     public class Event
     {
-        public int EventId;
-        public string EventName;
-        public int? EventCategoryId;
-        public string Description;
-        public DateTime? EventDate;
-        public int? VenueId;
-        public int? CommitteeId;
+        public int EventId { get; set; } 
+        public int ItecId { get; set; }  //Foreign
+        public string ItecYear { get; set; } 
+        public string EventName { get; set; }
+        public int EventCategoryId { get; set; }  //Foreign
+        public string? EventCategoryName { get; set; }
+        public string? Description { get; set; }
+        public DateTime EventDate { get; set; }
+        public int VenueId { get; set; } //Foreign
+        public string? VenueName { get; set; }
+        public int CommitteeId { get; set; } //Foreign
+        public string? CommitteeName { get; set; }
+      
+         
     }
 }
